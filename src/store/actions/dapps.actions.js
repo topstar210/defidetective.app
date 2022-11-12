107e5b349c70e33b0dfc04b310662b002f668edc
spaceboy000 <cryptospaceboy00@gmail.com>
2022-11-12 18:24:50 +0900

    calc TVL function based on scan api

--- src/store/actions/dapps.actions.js
+++ src/store/actions/dapps.actions.js
@@ -4,6 +4,10 @@ import {
 } from "../types/dapps.types";
 
 import myaxios from "src/provider/API"
+// import Web3 from 'web3';
+import axios from 'axios';
+import { config } from "../../config";
+import { redirect } from "react-router-dom";
 
 export const getDppList  = () => dispatch =>
 {
@@ -15,8 +19,47 @@ export const getDppList  = () => dispatch =>
     })
 }
 
-export const calcTVL = () => {
-    return '$316,084.76';
+export const calcTVL = async (chainId, tokenPrice, contractAddress, tokenKind) => {
+    contractAddress = contractAddress.slice(contractAddress.length-42);
+    let res, balance;
+    if (chainId == 'bsc') {
+        if (tokenKind.toLowerCase() == 'bnb') {
+            res = await axios.get(`https://api.bscscan.com/api?module=account&action=balance&address=${contractAddress}&tag=latest&apikey=${config.BSC_API_KEY}`);
+            balance =  res.data.result / Math.pow(10, 18);
+            return balance * tokenPrice;
+        } else if (tokenKind.toLowerCase() == 'busd') {
+            res = await axios.get(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&address=${contractAddress}&tag=latest&apikey=${config.BSC_API_KEY}`);
+            return res.data.result / Math.pow(10, 18);
+        } else if (tokenKind.toLowerCase() == 'usdt') {
+            res = await axios.get(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x55d398326f99059fF775485246999027B3197955&address=${contractAddress}&tag=latest&apikey=YGKJFMK5FW1H9T9GR9VTGIT2UC5PXUTDTB`);
+            return res.data.result / Math.pow(10, 18);
+        } else {
+            return 0;
+        }
+    } else if (chainId == 'polygon') {
+        if (tokenKind.toLowerCase() == 'matic') {
+            res = await axios.get(`https://api.polygonscan.com/api?module=account&action=balance&address=${contractAddress}&tag=latest&apikey=${config.POLYGON_API_KEY}`);
+            balance = res.data.result / Math.pow(10, 18);
+            return balance * tokenPrice;
+        } else if (tokenKind.toLowerCase() == 'usdt') {
+            res = await axios.get(`https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0xc2132D05D31c914a87C6611C10748AEb04B58e8F&address=${contractAddress}&tag=latest&apikey=${config.POLYGON_API_KEY}`);
+            return res.data.result / Math.pow(10, 6);
+        } else {
+            return 0;
+        }
+    } else if (chainId == 'avax') {
+        if (tokenKind.toLowerCase() == 'avax') {
+            res = await axios.get(`https://api.snowtrace.io/api?module=account&action=balance&address=${contractAddress}&tag=latest&apikey=ZVI4N9MEVBXDANDD4NPSXQI2NZEC9SYESU`);
+            balance = res.data.result / Math.pow(10, 18);
+            return balance * tokenPrice;
+        } else if (tokenKind.toLowerCase() == 'usdt') {
+        
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
 
 export const saveAppInfo = (data) => dispatch => {
--- src/views/dapps/Dapps.js
+++ src/views/dapps/Dapps.js
@@ -13,6 +13,8 @@ import {
 import { cilPlus, cibExpertsExchange, cilPen } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 import moment from 'moment';
+import axios from "axios";
+import Web3 from 'web3';
 
 import { 
   getDppList, 
@@ -22,6 +24,8 @@ import {
 } from "src/store/actions/dapps.actions"
 import Modal from "./components/Modal"
 import "./dapp.scss";
+import { config } from "../../config";
+import tradeJoeRounterAbi from "../../abis/tradeJoeRouterAbi.json";
 
 // table header
 const columns = [
@@ -48,8 +52,33 @@ const Dapps = () => {
   const { loginState } = useSelector(state => state.sapp);
   const [mVisible, setMVisible] = useState(false); // modal visible state;
 
+  const [bnbPrice, setBnbPrice] = useState(0);
+  const [maticPrice, setMaticPrice] = useState(0);
+  const [avaxPrice, setAvaxPrice] = useState(0);
+
   // initial data  ---------------
-  useEffect(() => { dispatch(getDppList()) }, []);
+  useEffect(() => {
+     dispatch(getDppList());
+     const getTokenPrice = async () => {
+      let res = await axios.get(`https://api.bscscan.com/api?module=stats&action=bnbprice&apikey=${config.BSC_API_KEY}`);
+      const bnbPrice = res.data.result.ethusd;
+      setBnbPrice(bnbPrice);
+      console.log('BNB Price: ', bnbPrice);
+
+      res = await axios.get(`https://api.polygonscan.com/api?module=stats&action=maticprice&apikey=${config.POLYGON_API_KEY}`);
+      const maticPrice = res.data.result.maticusd;
+      setMaticPrice(maticPrice);
+      console.log("Matic Price: ", maticPrice);
+
+      const Web3Instance = new Web3('https://api.avax.network/ext/bc/C/rpc');
+      const TradeJoeRounterContract = new Web3Instance.eth.Contract(tradeJoeRounterAbi, config.TradeJoeRouterAddress);
+      const avaxPrice = await TradeJoeRounterContract.methods.getAmountsOut(Web3.utils.toWei('1'), ["0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7","0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E"]).call();
+      setAvaxPrice(avaxPrice[1] / 1000000);
+      console.log('Avax Price: ', avaxPrice[1] / 1000000);
+     };
+     
+     getTokenPrice();
+  }, []);
   const { dappList } = useSelector(state => state.dapps);
 
   // if admin, add the actions
@@ -82,73 +111,89 @@ const Dapps = () => {
   }
 
   // making table rows
-  const items = dappList.map((val, ind) => {
-    appData[val.id] = val;
-
-    let splitbar = "";
-    if (dappList[ind + 1] && val.level !== dappList[ind + 1].level) {
-      splitbar = "split-row-" + val.level;
-    }
-    let resList = {
-      logo: <div style={{ width: 50, height: 50 }}><img src={val.logo_url} alt="" /></div>,
-      website: <CLink
-            className="website_link"
+  const [items, setItems] = useState([]);
+  useEffect(()=>{
+    let items = [];
+    const getItems = async()=>{
+      await Promise.all(dappList.map(async (val, ind) => {
+        appData[val.id] = val;
+        const chainId = val.contract.includes('bsc') ? 'bsc' : (val.contract.includes('polygon') ? 'polygon' : (val.contract.includes('snowtrace') ? 'avax' : 'undefined'));
+        const tokenPrice = chainId == 'bsc' ? bnbPrice : (chainId == 'polygon' ? maticPrice : (chainId == 'avax' ? avaxPrice : 0));
+        console.log("chainId: ", chainId, tokenPrice);
+        let splitbar = "";
+        if (dappList[ind + 1] && val.level !== dappList[ind + 1].level) {
+          splitbar = "split-row-" + val.level;
+        }
+        
+        const tvl = await calcTVL(chainId, tokenPrice, val.contract, val.coin_token);
+        let item = {
+          logo: <div style={{ width: 50, height: 50 }}><img src={val.logo_url} alt="" /></div>,
+          website: <CLink
+                className="website_link"
+                target="_blank"
+                href={val.mining_group_url}
+              >{val.mining_group}</CLink>,
+          defi_badge: <CLink
             target="_blank"
-            href={val.mining_group_url}
-          >{val.mining_group}</CLink>,
-      defi_badge: <CLink
-        target="_blank"
-        href={val.kyc}
-      >
-        <span className="badge bg-success-gradient">{val.kyc && "defi badge"}</span>
-      </CLink>,
-      telegram: <CLink
-        target="_blank"
-        href={val.tg_group}
-      >
-        <span className="badge bg-success-gradient">{val.tg_group && "telegram"}</span>
-      </CLink>,
-      discord: <CLink
-        target="_blank"
-        href={val.discode_link}
-      >
-        <span className="badge bg-success-gradient">{val.discode_link && "discord"}</span>
-      </CLink>,
-      twitter: <CLink
-        target="_blank"
-        href={val.twitter_link}
-      >
-        <span className="badge bg-success-gradient">{val.twitter_link && "twitter"}</span>
-      </CLink>,
-      token: val.coin_token ? val.coin_token : " ",
-      contract: <CLink
-        target="_blank"
-        href={val.contract}
-      >
-        <span className="badge bg-success-gradient">{val.contract && "contract"}</span>
-      </CLink>,
-      audit: <CLink
-        target="_blank"
-        href={val.audit}
-      >
-        <span className="badge bg-success-gradient">{val.audit && "audit"}</span>
-      </CLink>,
-      fees: val.fees ? val.fees : " ",
-      age: val.ages ? moment(val.ages, "").fromNow(true) : " ",
-      daily_percent: val.daily ? val.daily : " ",
-      tvl: calcTVL(),
-      _props: {
-        className: "level_" + val.level + " " + splitbar
-      }
+            href={val.kyc}
+          >
+            <span className="badge bg-success-gradient">{val.kyc && "defi badge"}</span>
+          </CLink>,
+          telegram: <CLink
+            target="_blank"
+            href={val.tg_group}
+          >
+            <span className="badge bg-success-gradient">{val.tg_group && "telegram"}</span>
+          </CLink>,
+          discord: <CLink
+            target="_blank"
+            href={val.discode_link}
+          >
+            <span className="badge bg-success-gradient">{val.discode_link && "discord"}</span>
+          </CLink>,
+          twitter: <CLink
+            target="_blank"
+            href={val.twitter_link}
+          >
+            <span className="badge bg-success-gradient">{val.twitter_link && "twitter"}</span>
+          </CLink>,
+          token: val.coin_token ? val.coin_token : " ",
+          contract: <CLink
+            target="_blank"
+            href={val.contract}
+          >
+            <span className="badge bg-success-gradient">{val.contract && "contract"}</span>
+          </CLink>,
+          audit: <CLink
+            target="_blank"
+            href={val.audit}
+          >
+            <span className="badge bg-success-gradient">{val.audit && "audit"}</span>
+          </CLink>,
+          fees: val.fees ? val.fees : " ",
+          age: val.ages ? moment(val.ages, "").fromNow(true) : " ",
+          daily_percent: val.daily ? val.daily : " ",
+          tvl: '$' + tvl.toFixed(2),
+          _props: {
+            className: "level_" + val.level + " " + splitbar
+          }
+        }
+    
+        if(loginState === "success" && columns[columns.length-1]['key'] === "action"){
+          item['action'] = <>
+            <CIcon onClick={()=>handleClickActions(val.id, 'E')} icon={ cilPen } className="text-white" size="sm" /> | &nbsp;
+            <CIcon onClick={()=>handleClickActions(val.id, 'D')} icon={ cibExpertsExchange } className="text-white" size="sm" />  
+          </>
+        }
+        items.push(item);
+      }));
+      console.log(items)
+      setItems([
+        ...items
+      ]);
     }
-    if(loginState === "success" && columns[columns.length-1]['key'] === "action"){
-      resList['action'] = <>
-        <CIcon onClick={()=>handleClickActions(val.id, 'E')} icon={ cilPen } className="text-white" size="sm" /> | &nbsp;
-        <CIcon onClick={()=>handleClickActions(val.id, 'D')} icon={ cibExpertsExchange } className="text-white" size="sm" />  
-      </>
-    }
-    return resList;
-  })
+    getItems();
+  },[dappList])
 
   return (
     <div className='dapps-page'>
